df27553fa8c99c775a716b9ecf32f4643430a47f
deimantekava <dkavaliauskait@gmail.com>
2023-04-27 19:46:17 +0300

    add 5 task

--- JS Tasks/Task 5/script.js
+++ JS Tasks/Task 5/script.js
@@ -19,3 +19,9 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+const pets = users.filter((user) => user.hasDog === true);
+console.log(pets);
+
+const minors = users.filter((user) => user.age > 18);
+console.log(minors);
