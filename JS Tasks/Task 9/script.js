49d965b11ee1f5ba44b0f7b41d52a0dc86532d7d
deimantekava <dkavaliauskait@gmail.com>
2023-04-27 21:20:18 +0300

    add 8 task

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -9,3 +9,26 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+function Movie() {
+  class Movies {
+    constructor(title, director, budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;
+    }
+    wasExpensive() {
+      if (this.budget > 100000000) {
+        console.log('True');
+      } else {
+        console.log('False');
+      }
+    }
+  }
+
+  const newMovie = new Movies('xxx', 'xxx', 200000000);
+  console.log(newMovie);
+  newMovie.wasExpensive();
+}
+
+Movie();
