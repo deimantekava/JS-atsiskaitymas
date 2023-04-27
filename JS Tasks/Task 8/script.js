1aafdd2273533d865611e76dfc47935f02e21953
deimantekava <dkavaliauskait@gmail.com>
2023-04-27 20:51:19 +0300

    add 9 task

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,14 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function calculator(a, b) {
+  const sum = a + b;
+  const subtraction = a - b;
+  const multiplication = a * b;
+  const division = a / b;
+  console.log(
+    `Sum: ${sum}, Subtraction: ${subtraction}, Multiplication: ${multiplication}, Division:  ${division}`
+  );
+}
+calculator(1, 2);
